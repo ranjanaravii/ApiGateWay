dc7d104f910e96fab85df57a29adf026e48a6320
raviranjankumar <raviranjan.kumar@lenskart.com>
2025-07-01 23:21:31 +0530

    initial commit

---
+++ src/test/java/com/developer/ApiGateWay/ApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.ApiGateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
